a6aa578f82165aa4c565a3b57d60c6f58d073b4b
Rodion Efremov <coderodd3@yandex.com>
2024-08-06 15:15:14 +0300

    ...

--- src/main/java/com/github/coderodde/game/chess/duel/ChessDuel.java
+++ src/main/java/com/github/coderodde/game/chess/duel/ChessDuel.java
@@ -2,10 +2,10 @@ package com.github.coderodde.game.chess.duel;
 
 import com.github.coderodde.game.chess.AbstractGameEngine;
 import com.github.coderodde.game.chess.ChessBoardState;
-import com.github.coderodde.game.chess.HeuristicFunction;
+import com.github.coderodde.game.chess.AbstractHeuristicFunction;
 import com.github.coderodde.game.chess.PlayerTurn;
 import com.github.coderodde.game.chess.ThreeFoldRepetionRuleDrawException;
-import com.github.coderodde.game.chess.impl.DefaultHeuristicFunction;
+import com.github.coderodde.game.chess.impl.ShannonHeuristicFunction;
 import com.github.coderodde.game.chess.impl.engine.AlphaBetaPruningGameEngine;
 
 /**
@@ -20,19 +20,23 @@ public class ChessDuel {
 
     public static void main(String[] args) {
         
-        final HeuristicFunction heuristicFunction =
-                new DefaultHeuristicFunction();
+        final AbstractHeuristicFunction heuristicFunction =
+                new ShannonHeuristicFunction();
         
         final AbstractGameEngine engine = 
                 new AlphaBetaPruningGameEngine(heuristicFunction);
         
         ChessBoardState state = new ChessBoardState();
-        PlayerTurn currentPlayerTurn = PlayerTurn.WHITE;
+
+        System.out.println(state);
         
         final Parameters parameters = Parameters.parseArguments(args);
         
-        parameters.whitePlayerEngineDepth = 4;
-        parameters.blackPlayerEngineDepth = 4;
+        parameters.whitePlayerEngineDepth = 3;
+        parameters.blackPlayerEngineDepth = 2;
+        
+        long totalDurationMillisWhite = 0L;
+        long totalDurationMillisBlack = 0L;
         
         System.out.printf("[INFO] White player engine depth: %d.\n",
                           parameters.whitePlayerEngineDepth);
@@ -43,84 +47,81 @@ public class ChessDuel {
         int plyNumber = 1;
         
         while (true) {
-            final String currentPlayerColorName = 
-                    getPlayerColorName(currentPlayerTurn);
             
-            System.out.printf("[STATUS] Ply number: %d by %s player bot.\n", 
-                              plyNumber++, 
-                              currentPlayerColorName);
+            System.out.printf("[STATUS] Ply number %d by white player bot:\n", 
+                              plyNumber++);
             
-            System.out.println(state);
-            
-            final PlayerTurn oppositePlayerTurn = 
-                    flipCurrentPlayerTurn(currentPlayerTurn);
-            
-            if (state.isCheckMate(oppositePlayerTurn)) {
-                final String playerColorName = 
-                        getPlayerColorName(currentPlayerTurn);
-                
-                System.out.printf("[STATUS] %s player won!\n",
-                                  playerColorName);
-                return;
-            }
-            
-            final long computationStartTime = System.currentTimeMillis();
+            long computationStartTimeMillis = System.currentTimeMillis();
             
             try {
-                final int depth = currentPlayerTurn == PlayerTurn.WHITE ?
-                        parameters.whitePlayerEngineDepth :
-                        parameters.blackPlayerEngineDepth;
-              
                 state = engine.search(state, 
-                                      depth, 
-                                      currentPlayerTurn);
+                                      parameters.whitePlayerEngineDepth,
+                                      PlayerTurn.WHITE);
                 
             } catch (final ThreeFoldRepetionRuleDrawException ex) {
                 System.out.println(
                         "[STATUS] Three-fold repetition rule "
                         + "broken. It's a draw!");
                 
-                return;
+                break;
             }
             
-            if (state == null) {
-                throw new IllegalStateException("state == null");
+            System.out.println(state);
+            
+            long computationEndTimeMillis = System.currentTimeMillis();
+            long computationTimeMillis = computationEndTimeMillis 
+                                       - computationStartTimeMillis;
+            
+            totalDurationMillisWhite += computationTimeMillis;
+            
+            System.out.printf("[STATUS] White AI bot ply in %d milliseconds.\n",
+                              computationTimeMillis);
+            
+            if (state.isCheckMate(PlayerTurn.BLACK)) {
+                System.out.println("[STATUS] White player won!");
+                break;
             }
             
-            final long computationEndTime = System.currentTimeMillis();
-            final long computationDuration = computationEndTime - 
-                                             computationStartTime;
+            System.out.printf("[STATUS] Ply number %d by black player bot:\n",
+                              plyNumber++);
             
-//            if (computationDuration < 500) {
-//                currentDepth++;
-//            } else if (computationDuration > 5000) {
-//                currentDepth = Math.max(2, currentDepth - 1);
-//            }
+            computationStartTimeMillis = System.currentTimeMillis();
             
-            System.out.printf("[INFO] %s player ply in %d milliseconds.\n",
-                              currentPlayerColorName,
-                              computationDuration);
+            try {
+                state = engine.search(state,
+                                      parameters.blackPlayerEngineDepth, 
+                                      PlayerTurn.BLACK);
+                
+            } catch (final ThreeFoldRepetionRuleDrawException ex) {
+                System.out.println(
+                        "[STATUS] Three-fold repetition rule "
+                        + "broken. It's a draw!");
+                
+                break;
+            }
             
-            currentPlayerTurn = flipCurrentPlayerTurn(currentPlayerTurn);
+            System.out.println(state);
+            
+            computationEndTimeMillis = System.currentTimeMillis();
+            computationTimeMillis = computationEndTimeMillis 
+                                  - computationStartTimeMillis;
+            
+            totalDurationMillisBlack += computationTimeMillis;
+            
+            System.out.printf("[STATUS] Black AI bot ply in %d milliseconds.\n",
+                              computationTimeMillis);
+            
+            if (state.isCheckMate(PlayerTurn.WHITE)) {
+                System.out.println("[STATUS] Black player won!");
+                break;
+            }
         }
-    }
-    
-    private static PlayerTurn 
-        flipCurrentPlayerTurn(final PlayerTurn currentPlayerTurn) {
-        return currentPlayerTurn == PlayerTurn.WHITE ?
-                PlayerTurn.BLACK :
-                PlayerTurn.WHITE;
-    }
         
-    private static String 
-        getPlayerColorName(final PlayerTurn currentPlayerTurn) {
-        if (currentPlayerTurn == PlayerTurn.WHITE) {
-            return "White";
-        } else if (currentPlayerTurn == PlayerTurn.BLACK) {
-            return "Black";
-        } else {
-            throw new IllegalStateException("Should not get here.");
-        }
+        System.out.printf("White AI bot total duration: %d milliseconds.\n",
+                          totalDurationMillisWhite);
+        
+        System.out.printf("Black AI bot total duration: %d milliseconds.\n",
+                          totalDurationMillisBlack);
     }
 }
 
